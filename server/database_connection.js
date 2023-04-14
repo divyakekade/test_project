b83716c2859840ea4641a3515008b641bb26ff4f
Divya Milind Kekade <kekadedivya@gmail.com>
2023-04-14 07:42:51 +0530

    docker images built

--- server/database_connection.js
+++ server/database_connection.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose');
 
 connectDatabase = () => {
-    url="mongodb+srv://divya_admin:divya2933@cluster0.vk1tkhq.mongodb.net/?retryWrites=true&w=majority";
+    // url="mongodb+srv://divya_admin:divya2933@cluster0.vk1tkhq.mongodb.net/?retryWrites=true&w=majority";
+    url="mongodb://admin:123456@mongodb:27017" 
     mongoose.connect(url).then(console.log("Database connected successfully")).catch((error) => {console.log(error)})
 } 
 
